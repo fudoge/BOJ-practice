92c51f6e36fef12ea38dcfc343d249c84d02c9c7
fudoge <null>
2022-09-27 14:46:38 +0000

    스택(22.09.27)

---
+++ Stack_using_linked_list.py
@@ -0,0 +1,29 @@
+class Node():	#노드 선언
+	def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+class Stack():
+    def __init__(self):
+        self.head = None
+    
+    def isEmpty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+    
+    def push(data):
+        if self.isEmpty():
+            self.head = Node(data)
+        else:
+            self.head = Node(data, self.head)
+        
+    def pop():
+        headNode = self.head
+        self.head = self.head.next
+        headNode.next = None
+        return headNode
+    
+    def top():
+        return self.head
