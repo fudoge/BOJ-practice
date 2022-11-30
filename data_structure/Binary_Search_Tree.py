7e99084570753f97991d88ecc2b057df5657250d
fudoge <null>
2022-11-30 14:45:42 +0000

    BST구현 수정_('22.11.30)

--- data_structure/Binary_Search_Tree.py
+++ data_structure/Binary_Search_Tree.py
@@ -8,41 +8,40 @@ class Tree():
     def __init__(self):
         self.root = None
         
-    def _search(self, key):
-        currentNode = self.root
-        while currentNode:
-            if currentNode.key == key:
-                return True
-            elif currentNode.key > key:
-                if currentNode.left:
-                    currentNode = currentNode.left
-                else:
-                    return False
-            else:
-                if currentNode.right:
-                    currentNode = currentNode.right
-                else:
-                    return False
+    def _search(self, root, key):    #탐색        
+        if root == None:             #만약 root값이 없으면: (찾지 못한 경우)
+            return "Not Found!"
+        if root.key == key:          #값을 찾으면:
+            return root.key
+        elif root.key > key:         #root가 더 크다면: (왼쪽으로 이동하는 재귀)
+            return self._search(root.left, key)
+        else:                        #root가 더 작다면: (오른쪽으로 이동하는 재귀)
+            return self._search(root.right, key)
                 
     def _insert(self, key):
-        currentNode = self.root
-        while currentNode:
-            if currentNode.key == key:
-                print("data ", key, " is already exist!")
+        def _addNode(root, key):    #노드 추가 정의
+            if key == root.key:     #루트와 같은 key라면, False반환
                 return False
-            elif currentNode.key > key:
-                if currentNode.left:
-                    currentNode = currentNode.left
-                else:
-                    currentNode.left = Node(key)
-                    return
-            else:
-                if currentNode.right:
-                    currentNode = currentNode.right
-                else:
-                    currentNode.right = Node(key)
-                    return
-                    
+            elif key < root.key:    #루트보다 작은데..
+                if root.left == None:#루트의 왼쪽이 비었다면? 바로 왼쪽에 삽입하면된다.
+                    root.left = Node(key)
+                    return True
+                else:                #아니라면, 루트의 왼쪽 서브트리를 루트로 하고, 재귀적으로 실행시킨다.
+                    _addNode(root.left, key)
+            else:                    #루트보다 큰데..
+                if root.right == None:#루트의 오른쪽이 비었다면? 바로 오른쪽에 삽입하면된다.
+                    root.right = Node(key)
+                    return True
+                else:                #아니라면, 루트의 오른쪽 서브트리를 루트로 하고, 재귀적으로 실행한다.
+                    _addNode(root.right, key)        
+        if self.root == None:        #만약 루트가 없으면, 루트부터 만들어주자
+            self.root = Node(key)
+            return True
+        else:                        #루트가 없으면? 정의해놓은 노드 추가를 이용하자.
+            _addNode(self.root, key)
+            
+    
+    
     def _delete(self, key):
         parentNode = None
         currentNode = self.root
@@ -68,8 +67,6 @@ class Tree():
                 parentOfmin = minNode
                 minNode = minNode.left
                 isright = False
-                print("민노드는, 패런트 ", minNode.key,",", parentOfmin.key)
-            print("민노드는, 패런트 ", minNode.key,",", parentOfmin.key)
             currentNode.key = minNode.key
             if isright is True:
                 parentOfmin.right = None
@@ -89,6 +86,10 @@ class Tree():
                 parentNode.left = None
             else:
                 parentNode.right = None
+                
+    def _delete2(self, key):
+
+        
             
     def inorder_traversal(self, node):    #중위 순회
         if node.left:
@@ -108,10 +109,12 @@ BST._insert(9)
 BST._insert(8)
 BST._insert(5)
 
+print(BST._search(BST.root, 5))
+
 BST.inorder_traversal(BST.root)
 print()
 
-BST._delete(11)
+BST._delete2(10)
 
 BST.inorder_traversal(BST.root)
 
